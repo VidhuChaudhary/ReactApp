9b0fa0262724c965c8e36d3e3260a432d929b8ca
swati <deol.swati@gmail.com>
2020-03-29 23:03:38 +0530

    Updates

---
+++ precache-manifest.07c182a7d92c1b2e7a5275cc561c033d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4d8dd29191646e9362928970989d67",
+    "url": "/VidhuChaudhary/ReactApp.git/index.html"
+  },
+  {
+    "revision": "e34bdf9c920202c83512",
+    "url": "/VidhuChaudhary/ReactApp.git/static/css/main.4e68f0b1.chunk.css"
+  },
+  {
+    "revision": "ee5118fd300373a64740",
+    "url": "/VidhuChaudhary/ReactApp.git/static/js/2.e49bd680.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/VidhuChaudhary/ReactApp.git/static/js/2.e49bd680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34bdf9c920202c83512",
+    "url": "/VidhuChaudhary/ReactApp.git/static/js/main.72fbc9d8.chunk.js"
+  },
+  {
+    "revision": "8a9886f7c4e7ebbab12d",
+    "url": "/VidhuChaudhary/ReactApp.git/static/js/runtime-main.6479a725.js"
+  }
+]);
